e475a522121742d834e5a20da2ddf7b8fe37db05
akhi jannat <ajjannat02@gmail.com>
2023-09-22 09:53:41 +0600

    design update

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,11 +1,25 @@
 <script setup>
-import Hero from "../components/Hero.vue";
+import {RouterLink,RouterView } from 'vue-router'
+import axios from 'axios'
+import { ref, onBeforeMount } from 'vue'
+const products = ref([])
+
+
+onBeforeMount(() => {
+  axios.get('https://dummyjson.com/products')
+      .then(res => {
+        products.value = res.data.products
+      })
+});
+
 
 const detailsBtn =
-  "text-right border border-lime-600 font-semibold transition duration-200 rounded-sm py-1 px-3 text-lime-600 hover:shadow-xl";
+    "text-right border border-lime-600 font-semibold transition duration-200 rounded-sm py-1 px-3 text-lime-600 hover:shadow-xl";
+
+
+
 </script>
 <template>
-  <Hero />
   <section class="container mx-auto py-20 px-10">
     <h1 class="text-2xl md:text-6xl font-semibold pb-2 text-center">
       Featured Plants
@@ -15,174 +29,20 @@ const detailsBtn =
     </p>
 
     <div class="h-1 bg-lime-600 w-52 mx-auto my-3"></div>
-
     <div class="grid grid-cols-1 md:grid-cols-4 gap-5 mt-5 md:mt-10">
-      <div class="border hover:shadow-lg cursor-pointer">
+      <div class="border hover:shadow-lg cursor-pointer" v-for="product in products" :key="product.id">
         <img
-          src="../assets/plant1-free-img.jpg"
+          :src="product.thumbnail"
           class="h-[300px] w-auto"
           alt=""
         />
         <div class="p-5">
-          <span class="text-gray-500 italic text-sm">Plant</span>
-          <h4>Boncellensis Secullant</h4>
-          <p>$34.00</p>
-          <div class="flex justify-between items-center">
-            <div class="flex items-center mt-2">
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-            </div>
-            <button :class="detailsBtn" type="submit">View Details</button>
-          </div>
-        </div>
-      </div>
+          <span class="text-gray-800 italic text-sm font-bold">{{product.title}}</span>
+          <p>$ {{product.price}}</p>
+          <div class="flex justify-between items-center mt-4">
+            <router-link :to="{ name: 'productDetails', params: { id: product.id } }" :class="detailsBtn" type="submit">View Details </router-link>
 
-      <div class="border hover:shadow-lg cursor-pointer">
-        <img
-          src="../assets/plant3-free-img.jpg"
-          class="h-[300px] w-auto"
-          alt=""
-        />
-        <div class="p-5">
-          <span class="text-gray-500 italic text-sm">Plant</span>
-          <h4>Boncellensis Secullant</h4>
-          <p>$34.00</p>
-          <div class="flex justify-between items-center">
-            <div class="flex items-center mt-2">
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-            </div>
-            <button :class="detailsBtn" type="submit">View Details</button>
-          </div>
-        </div>
-      </div>
-      <div class="border hover:shadow-lg cursor-pointer">
-        <img
-          src="../assets/plant4-free-img.jpg"
-          class="h-[300px] w-auto"
-          alt=""
-        />
-        <div class="p-5">
-          <span class="text-gray-500 italic text-sm">Plant</span>
-          <h4>Boncellensis Secullant</h4>
-          <p>$34.00</p>
-          <div class="flex justify-between items-center">
-            <div class="flex items-center mt-2">
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-            </div>
-            <button :class="detailsBtn" type="submit">View Details</button>
-          </div>
-        </div>
-      </div>
-      <div class="border hover:shadow-lg cursor-pointer">
-        <img
-          src="../assets/plant5-free-img.jpg"
-          class="h-[300px] w-auto"
-          alt=""
-        />
-        <div class="p-5">
-          <span class="text-gray-500 italic text-sm">Plant</span>
-          <h4>Boncellensis Secullant</h4>
-          <p>$34.00</p>
-          <div class="flex justify-between items-center">
-            <div class="flex items-center mt-2">
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-            </div>
-            <button :class="detailsBtn" type="submit">View Details</button>
-          </div>
-        </div>
-      </div>
-      <div class="border hover:shadow-lg cursor-pointer">
-        <img
-          src="../assets/plant6-free-img.jpg"
-          class="h-[300px] w-auto"
-          alt=""
-        />
-        <div class="p-5">
-          <span class="text-gray-500 italic text-sm">Plant</span>
-          <h4>Boncellensis Secullant</h4>
-          <p>$34.00</p>
-          <div class="flex justify-between items-center">
-            <div class="flex items-center mt-2">
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-            </div>
-            <button :class="detailsBtn" type="submit">View Details</button>
-          </div>
-        </div>
-      </div>
-      <div class="border hover:shadow-lg cursor-pointer">
-        <img
-          src="../assets/cactus2-free-img.jpg"
-          class="h-[300px] w-auto"
-          alt=""
-        />
-        <div class="p-5">
-          <span class="text-gray-500 italic text-sm">Plant</span>
-          <h4>Boncellensis Secullant</h4>
-          <p>$34.00</p>
-          <div class="flex justify-between items-center">
-            <div class="flex items-center mt-2">
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-            </div>
-            <button :class="detailsBtn" type="submit">View Details</button>
-          </div>
-        </div>
-      </div>
-      <div class="border hover:shadow-lg cursor-pointer">
-        <img
-          src="../assets/cactus4-free-img.jpg"
-          class="h-[300px] w-auto"
-          alt=""
-        />
-        <div class="p-5">
-          <span class="text-gray-500 italic text-sm">Plant</span>
-          <h4>Boncellensis Secullant</h4>
-          <p>$34.00</p>
-          <div class="flex justify-between items-center">
-            <div class="flex items-center mt-2">
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-            </div>
-            <button :class="detailsBtn" type="submit">View Details</button>
-          </div>
-        </div>
-      </div>
-      <div class="border hover:shadow-lg cursor-pointer">
-        <img
-          src="../assets/cactus6-free-img.jpg"
-          class="h-[300px] w-auto"
-          alt=""
-        />
-        <div class="p-5">
-          <span class="text-gray-500 italic text-sm">Plant</span>
-          <h4>Boncellensis Secullant</h4>
-          <p>$34.00</p>
-          <div class="flex justify-between items-center">
-            <div class="flex items-center mt-2">
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-              <img src="../assets/star.svg" class="h-4" alt="" />
-            </div>
-            <button :class="detailsBtn" type="submit">View Details</button>
+
           </div>
         </div>
       </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,39 @@
 import { createRouter, createWebHistory } from "vue-router";
-import ProductList from "../components/ProductList.vue";
+import HomeView from "../views/HomeView.vue";
 import ProductDetails from "../components/ProductDetails.vue";
-
-const routes = [
-  { path: "/", component: ProductList },
-  { path: "/product/:id", component: ProductDetails },
-];
+import AboutView from "../views/AboutView.vue";
+import ContactView from "../views/ContactView.vue";
+import StoreView from "../views/StoreView.vue";
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: AboutView,
+    },
+    {
+      path: "/contact",
+      name: "contact",
+      component: ContactView,
+    },
+    {
+      path: "/store",
+      name: "store",
+      component: StoreView,
+    },
+    {
+      path: "/single/product/:id",
+      name: "productDetails",
+      component: ProductDetails,
+    },
+  ],
 });
 
 export default router;
